e6218c98867b1eecadf035cf9b2031a78b42fa4e
Nicole Green <nicolegreen1991@hotmail.com>
2016-11-17 19:13:08 +0000

    Hello Deacon

---
+++ src/main/java/com/us/test/Main.java
@@ -0,0 +1,9 @@
+package com.us.test;
+
+public class Main {
+    public static void main(final String[] args) {
+        final Server server = new Server();
+        server.start();
+
+    }
+}
---
+++ src/main/java/com/us/test/Server.java
@@ -0,0 +1,7 @@
+package com.us.test;
+
+public class Server {
+    public void start(){
+        System.out.println("Hello Deacon");
+    }
+}
